8738c3ca5e813bc248c3b7770c53b9ff67677dca
cnluzon <cnluzon@gmail.com>
2023-05-02 14:31:14 +0200

    Update imported packages and examples

--- R/arithmetic.R
+++ R/arithmetic.R
@@ -7,9 +7,15 @@
 #'
 #' @return Integer representing the total bp overlap between two sets of loci.
 #' @importFrom GenomicRanges intersect width
+#' @importClassesFrom GenomicRanges GRanges
+#' @importClassesFrom IRanges IRanges
 #' @export
 #'
 #' @examples
+#' gr_1 <- GenomicRanges::GRanges(seqnames = c("chr1"), IRanges::IRanges(10, 20), strand = "-")
+#' gr_2 <- GenomicRanges::GRanges(seqnames = c("chr1"), IRanges::IRanges(15, 25), strand = "+")
+#' total_bp_overlap(gr_1, gr_2, ignore.strand = TRUE)
+#' total_bp_overlap(gr_1, gr_2, ignore.strand = FALSE)
 total_bp_overlap <- function(gr1, gr2, ignore.strand = TRUE) {
   sum(
     width(
