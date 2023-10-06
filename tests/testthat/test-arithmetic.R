cd84169f2d72de29cb92767cbdcd54e3f8b05f59
cnluzon <cnluzon@gmail.com>
2023-10-06 12:38:52 +0200

    Further testing for loci_overlap

--- tests/testthat/test-arithmetic.R
+++ tests/testthat/test-arithmetic.R
@@ -83,3 +83,25 @@ test_that("loci_overlap() returns correct values with assymetrical case", {
   expect_equal(overlap_b, 2)
 })
 
+test_that("loci_overlap() returns correct values with minoverlap", {
+  gr_1 <- GenomicRanges::GRanges(seqnames = c("chr1"), IRanges::IRanges(10, 20), strand = "-")
+  gr_2 <- GenomicRanges::GRanges(
+    seqnames = c("chr1", "chr1"), IRanges::IRanges(c(15, 19), c(18, 25)), strand = "+")
+
+  # This should skip [19-25] overlap with [10-20] as the length of that overlap is 2.
+  overlap <- loci_overlap(gr_2, gr_1, ignore.strand = TRUE, minoverlap = 3)
+  expect_equal(overlap, 1)
+})
+
+test_that("loci_overlap() returns correct values with minoverlap edge case", {
+  gr_1 <- GenomicRanges::GRanges(seqnames = c("chr1", "chr1"), IRanges::IRanges(c(10, 24), c(20, 30)), strand = "-")
+  gr_2 <- GenomicRanges::GRanges(seqnames = c("chr1"), IRanges::IRanges(18, 25), strand = "+")
+
+  # gr_2 [25, 30] overlaps 5bp with gr_1 in TOTAL, but 3, 2, respectively.
+  # This is an edge case example to showcase how minoverlap works on GRanges
+  # because documentation seems to suggest that it will look at total. Turns
+  # out, it does not, so results expected with minoverlap 3, only counts the
+  # first overlap.
+  overlap <- loci_overlap(gr_1, gr_2, ignore.strand = TRUE, minoverlap = 3)
+  expect_equal(overlap, 1)
+})
