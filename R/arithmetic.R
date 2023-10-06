191a6e0b3dd43b2e360da19c49533642e38aa021
cnluzon <cnluzon@gmail.com>
2023-10-06 11:12:50 +0200

    jaccard index function

--- R/arithmetic.R
+++ R/arithmetic.R
@@ -23,3 +23,31 @@ total_bp_overlap <- function(gr1, gr2, ignore.strand = TRUE) {
     )
   )
 }
+
+#' Calculate jaccard index between two GenomicRanges objects.
+#'
+#' Jaccard index = length(intersection) / length(union)
+#'
+#' @param gr1 GRanges object
+#' @param gr2 GRanges object
+#' @param ignore.strand If FALSE, only matching strand overlaps are counted
+#'
+#' @return Numeric value representing the jaccard intersection
+#' @importFrom GenomicRanges width
+#' @export
+#'
+#' @examples
+#' gr_1 <- GenomicRanges::GRanges(seqnames = c("chr1"), IRanges::IRanges(10, 20), strand = "-")
+#' gr_2 <- GenomicRanges::GRanges(seqnames = c("chr1"), IRanges::IRanges(15, 25), strand = "+")
+#' jaccard_index(gr_1, gr_2, ignore.strand = TRUE)
+jaccard_index <- function(gr1, gr2, ignore.strand = TRUE) {
+   gr1_len <- sum(GenomicRanges::width(gr1))
+   gr2_len <- sum(GenomicRanges::width(gr2))
+
+   bp_ovp <- total_bp_overlap(gr1, gr2, ignore.strand = ignore.strand)
+   jaccard <- bp_ovp / (gr1_len + gr2_len - bp_ovp)
+   jaccard
+}
+
+
+
--- tests/testthat/test-arithmetic.R
+++ tests/testthat/test-arithmetic.R
@@ -13,3 +13,17 @@ test_that("total_bp_overlap() ignores strand when ignore.strand = TRUE", {
   # [10, 20] intersection with [15, 25] is [15, 20] which length is 6
   expect_equal(total_bp_overlap(gr_1, gr_2, ignore.strand = TRUE), 6)
 })
+
+test_that("jaccard_index() calculates correct value", {
+  gr_1 <- GenomicRanges::GRanges(seqnames = c("chr1"), IRanges::IRanges(10, 20), strand = "-")
+  gr_2 <- GenomicRanges::GRanges(seqnames = c("chr1"), IRanges::IRanges(15, 25), strand = "+")
+
+  expect_equal(jaccard_index(gr_1, gr_2, ignore.strand = TRUE), 0.375)
+})
+
+test_that("jaccard_index() calculates correct value when ignore.strand = FALSE", {
+  gr_1 <- GenomicRanges::GRanges(seqnames = c("chr1"), IRanges::IRanges(10, 20), strand = "-")
+  gr_2 <- GenomicRanges::GRanges(seqnames = c("chr1"), IRanges::IRanges(15, 25), strand = "+")
+
+  expect_equal(jaccard_index(gr_1, gr_2, ignore.strand = FALSE), 0)
+})
